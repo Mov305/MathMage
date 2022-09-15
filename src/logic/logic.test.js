3fcba5015830c2c5aec7ec76c18b25645d25fde4
tamana-Balkhi <tamana.balkhi1377@gmail.com>
2022-09-15 19:19:20 +0430

    test calculate and operate

--- src/logic/logic.test.js
+++ src/logic/logic.test.js
@@ -1,17 +1,41 @@
 import calculate from "./calculate";
 import operate from "./operate";
 
-test("AC button", () => {
+
+test('AC button returns object props equal null', () => {
   const obj = {
-    total: "0",
-    next: "0",
-    operation: null,
-  };
-  const buttonName = "AC";
-  const result = calculate(obj, buttonName);
-  expect(result).toEqual({
-    total: null,
     next: null,
     operation: null,
+    total: null,
+  };
+  calculate({}, 'AC');
+  expect(obj.next).toBe(null);   
+});
+
+test('testing addition ', () => {
+  const obj = {
+    next:2,
+    operation: '+',
+    total: 5,
+  };
+  calculate({next: '2', operation: '+', total: '3'}, '+');
+  expect(obj.next).toBe(2);   
+});
+
+
+describe('Test operations with numbers', () => {
+  test('  x sign', () => {
+    expect(operate(5, 8, 'x')).toBe('40');
+  });
+
+  test('÷ sign', () => {
+    expect(operate(4, 2, '÷')).toBe('2');
+  });
+
+  test('+ sign', () => {
+    expect(operate(6, 4, '+')).toBe('10');
+  });
+   test("-" , () => {
+    expect(operate(6, 4, '-')).toBe('2');
   });
 });
